e7aa7ea930a05a3a59b50ce15702a43815de39c7
wildmanebot <wildmane@protonmail.com>
2022-11-25 18:42:02 -0500

    app.py modified with plotly chart and checkbox, comments added

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
+
+#read in required libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+#read in CSV file
 vehicles_df = pd.read_csv('vehicles_us.csv')
 
-st.header('Test')
+#header text
+st.header('A Closer Look at Drivetrain Types in the Used Vehicle Market')
 
-st.write('Test') 
+#Description of the purpose of the page
+st.write('We will take a closer look at the differences between 4wd and 2wd drivetrain type vehicles.') 
+
+#create df with average price vs condition of vehicles data
+pc_df = vehicles_df.groupby('condition')['price'].mean().round(0).astype('int')
+
+#plotly chart settings
+fig = px.bar(pc_df, y="price",
+title="Average Price of Vehicle listings versus their Condition", color="price",
+labels={"price": "Average Price ($)", "condition": "Condition"})
+fig.update_xaxes(categoryorder='array', categoryarray= ['new', 'like new', 'excellent', 'good', 'fair', 'salvage'])
+fig.update(layout_coloraxis_showscale=False)
+
+# showing the barchart of average price vs condition
+st.plotly_chart(fig, use_container_width=True)
+
+#create checkboxes
+cbox_4wd = st.checkbox('Display 4wd Vehicles')
+
+if cbox_4wd:
+    st.write('test')
+
+
